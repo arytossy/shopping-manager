6e40e418e776f1c8db838f4baf1d0b2f90575564
arytossy <arytossy@gmail.com>
2020-11-27 08:33:36 +0000

    Modify cloudder conf to accept env CLOUDINARY_URL

--- config/cloudder.php
+++ config/cloudder.php
@@ -1,5 +1,8 @@
 <?php
 
+// herokuのaddon対応
+sscanf(env('CLOUDINARY_URL'), 'cloudinary://%15s:%27s@%9s', $api_key, $api_secret, $cloud_name);
+
 return [
 
     /*
@@ -12,12 +15,12 @@ return [
     |
     */
 
-    'cloudName'  => env('CLOUDINARY_CLOUD_NAME'),
+    'cloudName'  => env('CLOUDINARY_CLOUD_NAME', $cloud_name),
     'baseUrl'    => env('CLOUDINARY_BASE_URL', 'http://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
     'secureUrl'  => env('CLOUDINARY_SECURE_URL', 'https://res.cloudinary.com/'.env('CLOUDINARY_CLOUD_NAME')),
     'apiBaseUrl' => env('CLOUDINARY_API_BASE_URL', 'https://api.cloudinary.com/v1_1/'.env('CLOUDINARY_CLOUD_NAME')),
-    'apiKey'     => env('CLOUDINARY_API_KEY'),
-    'apiSecret'  => env('CLOUDINARY_API_SECRET'),
+    'apiKey'     => env('CLOUDINARY_API_KEY', $api_key),
+    'apiSecret'  => env('CLOUDINARY_API_SECRET', $api_secret),
 
     'scaling'    => [
         'format' => 'png',
